db76f14846fd3e393305f6308ad6e5d09f5659d9
TalisMan701 <boss.kompaneec@mail.ru>
2021-11-20 15:19:37 +0500

    update landing

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import classes from './Landing.module.css'
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 
 const Landing = (props) => {
 	return (
 		<>
+			<Header/>
+			<main>
 
+			</main>
+			<Footer/>
 		</>
 	);
 };
