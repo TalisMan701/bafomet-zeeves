3bfa3cd71cac62a20ac7e7c92a431913aa0bfc42
Red-Stalker <dnovgor@mail.ru>
2021-11-20 15:27:36 +0500

    test1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import logo from './logo.svg';
 import './App.css';
+import "zeeves-auth-sdk-js";
+import {useEffect} from "react";
 
-function App() {
-  
+const App = () => {
+    
   return (
     <div>
 
